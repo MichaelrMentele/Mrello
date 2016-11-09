335582919b9b5cd770780bef2ffbf253980bbd6d
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-09 08:47:33 -0800

    moved client to separate app to serve and manage assets

--- client/public/javascripts/collections/cards.js
+++
@@ -1,8 +0,0 @@
-var MrelloApp = MrelloApp || {};
-
-MrelloApp.collection.Cards = Backbone.Collection.extend({
-  model: MrelloApp.model.Card,
-  parse: function(response) {
-    return response.cards;
-  }
-});
