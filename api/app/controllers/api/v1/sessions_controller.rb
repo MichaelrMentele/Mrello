96282edbd22555af11f2169a825646cbac543dcf
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-18 09:46:44 -0800

    supporting session creation

--- api/app/controllers/api/v1/sessions_controller.rb
+++ api/app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,25 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    session[:user_id] = params[:id]
-    render json: 
-      { status: 201, message: "User logged in.", user_id: params[:id] }, status: 201
+    binding.pry
+    user = User.find_by(email: session_params[:email])
+    if user.authenticate(session_params[:password])
+      render json: {
+        status: :created, 
+        message: "User logged in.", 
+        session_token: "SECRET JWT TOKEN I HAVEN\'t MADE YET"
+      }
+    else
+
+    end
   end
 
   def destroy
     session[:user_id] = nil
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
   end
 end
