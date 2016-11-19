cdcc9ca60f42dcee5a70f00226e07f7c515938a3
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-18 18:58:45 -0800

    tweak

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::API
 
   def authenticate_request
     @current_user = AuthenticateApiRequest.call(request.headers).result
-
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
 end
