d0130325dc8c6fdef05ff5acf8ee8a2a0558cf40
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-20 16:04:19 -0800

    remove pry

--- api/app/controllers/api/v1/organizations_controller.rb
+++ api/app/controllers/api/v1/organizations_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::OrganizationsController < ApplicationController
   before_action :authenticate_request
 
   def create
-    binding.pry
     user = User.find(params[:user_id])
     org = Organization.new(admin_id: params[:user_id], title: params[:title])
     if user.admin? and org.save
