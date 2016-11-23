9d9de1804c348e73e2f3cb376a8abf3e58781029
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-23 14:15:46 -0800

    refactor depraction with .to_h

--- api/app/controllers/api/v1/lists_controller.rb
+++ api/app/controllers/api/v1/lists_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ListsController < ApplicationController
   before_action :authenticate_request
   
   def create
-    @list = List.new(list_params.merge!(user_id: current_user.id))
+    @list = List.new(list_params.to_h.merge(user_id: current_user.id))
     if @list.save
       @message = "List created."
       render :create, status: :created
