a86f7accf13ba0d40b3252a5cd5d5b29f8a274cc
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-26 14:51:42 -0800

    fixed models

--- api/app/models/board.rb
+++ api/app/models/board.rb
@@ -1,7 +1,7 @@
 class Board < ApplicationRecord
-  validates_presence_of :owner_id
+  validates_presence_of :ownership_id
 
   has_many :lists
 
-  belongs_to :owner, class_name: "User", foreign_key: :owner_id
+  belongs_to :ownership
 end
--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -5,7 +5,9 @@ describe User do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   
-  it { is_expected.to have_one(:board) }
-  it { is_expected.to belong_to(:organization) }
-  it { is_expected.to have_many(:join_requests) }
+  it { is_expected.to have_many(:organizations) }
+  it { is_expected.to have_many(:memberships) }
+
+  it { is_expected.to have_many(:ownerships) }
+  it { is_expected.to have_many(:boards) }
 end
