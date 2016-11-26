9392c0ea40827aeefac9f1db04f0e162bda59040
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-25 19:49:24 -0800

    why

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123030339) do
+ActiveRecord::Schema.define(version: 20161126034017) do
+
+  create_table "boards", force: :cascade do |t|
+    t.integer  "admin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cards", force: :cascade do |t|
     t.integer  "list_id"
